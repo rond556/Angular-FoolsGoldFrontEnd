1037b1576b0715b1b95c33c669dcc6c56321b91c
Ronald Duwell <ron@Ronalds-MacBook-Pro.local>
2020-04-13 15:48:40 -0400

    building routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeckconstructComponent } from './deckconstruct/deckconstruct.component';
+import { DeckdisplayComponent } from './deckdisplay/deckdisplay.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'deckconstruct', component: DeckconstructComponent},
+  { path: 'deckdisplay', component: DeckdisplayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
